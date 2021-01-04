8f84d11591a7223cb2cfd0ff4825cc34483d8e69
dymafr <erwan1309@hotmail.com>
2021-01-04 17:06:42 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,6 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent implements OnInit {
-  public nom = "Jean";
-  public date1 = new Date();
-  public date2 = Date.now();
-
   constructor() {}
 
   ngOnInit(): void {}
