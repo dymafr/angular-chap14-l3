8d129a1287b850f4e8b7aa1d4ca009a5cddcdb38
dymafr <erwan1309@hotmail.com>
2021-01-04 17:16:04 +0100

    2 files modified

--- src/app/cb.pipe.ts
+++ src/app/cb.pipe.ts
@@ -2,7 +2,9 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({ name: "cb" })
 export class CbPipe implements PipeTransform {
-  transform(value: string): string {
-    return `${value.slice(0, 4)} xxxx xxxx ${value.slice(-4, value.length)}`;
+  transform(value: string, n: number = 4): string {
+    return `${value.slice(0, n)} ${new Array(17 - 2 * n).join(
+      "x"
+    )} ${value.slice(-n, value.length)}`;
   }
 }
